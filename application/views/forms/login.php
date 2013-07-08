d116a496218958e84486c843ff1859156163d494
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:09:02 -0700

    form login

---
+++ application/views/forms/login.php
@@ -0,0 +1,45 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */s
+
+$form = '<div class="bmms-forms">';
+
+$form .= bframework_view_label(array(
+	             'attributes' => array('for' => 'username'),
+				 'name' => bframework_print('login:username')
+				 ));
+				 
+$form .= bframework_view_input(array(
+	                         'name' => 'username',
+		                     'type' => 'text',
+							 'placeholder' => bframework_print('login:username'),
+		)).'<br />';
+		
+$form .= bframework_view_label(array(
+	             'attributes' => array('for' => 'password'),
+				 'name' => bframework_print('login:username')
+		));
+		
+$form .= bframework_view_input(array(
+	                         'name' => 'password',
+		                     'type' => 'password',
+							 'placeholder' => bframework_print('login:password'),
+		)).'<br />';
+		
+$form .= bframework_view_input(array(
+		                     'type' => 'submit',
+							 'class' => 'submit'
+		)).'<br />';
+
+
+$form .= '</div>';
+
+echo $form;
