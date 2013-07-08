d7a352cd25599e76744f1d0b8de9e64d689836c4
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:00:11 -0700

    action for logout

---
+++ application/actions/user/logout.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+if(isset($_SESSION['bmmsusername'])){
+unset($_SESSION['bmmsusername']);
+}
+redirect(1);
