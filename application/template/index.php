ecf1238fafc96217405c8b963379733b6337ffaa
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:07:27 -0700

    template.index

---
+++ application/template/index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+if(user_is()){
+   redirect('page/mails');
+}
+elseif(!user_is()){
+     redirect('page/login');
+}   
