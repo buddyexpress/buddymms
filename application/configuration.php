3f1f41f3c373275d7d7410a95b22f28566d94e3f
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:03:08 -0700

    config file

---
+++ application/configuration.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+$bmms_database_config = array(
+         'username' => 'database username', 
+         'password' => 'database password',
+		 'host' => 'database host',
+		 'dbname' => 'database name'
+);
