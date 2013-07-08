2806f38ade687a72b6ea40621f399af685111081
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:09:28 -0700

    forms.settings

---
+++ application/views/forms/settings.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+?> 		
+							<div class="bmms-forms">
+										<p>
+						<input name="name" type="text" value="<?php echo user_entity(); ?>" placeholder=" Name"  />
+								</p>
+										<p>
+											<input name="email" type="email" value="<?php echo user_entity('email'); ?>" placeholder=" E-Mail"  />
+										</p>
+	<p>
+											<input name="password" type="password" placeholder=" Passowrd" />
+										</p>
+										
+													</p>
+	                                     
+										
+										     <p>
+											<input type="submit" value="Save" class="submit"  />
+										</p>
+										</div>
+										
