4b72d9f9d5ba403f7e40fa253870f7ac7e7497cf
Arsalan <arsalan@buddyexpress.net>
2013-07-08 09:59:34 -0700

    action for delete api

---
+++ application/actions/deleteapi.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+$delete = new BMMS_APISTATS;
+$delete->delete_api($_POST['bmms_apis']);
+redirect('page/apis');
