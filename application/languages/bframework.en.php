aef2e77b2e8e6c59a5eef63bae512cd50cb555ae
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:04:46 -0700

    language file for bmms

---
+++ application/languages/bframework.en.php
@@ -0,0 +1,54 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+$englsih = array(
+         'api:add' => 'Add api key', 
+         'welcome:home' => 'Welcome to the website',
+         'list:apis' => 'Apis list',
+		 'api:stat' => 'Api Stat',
+		 'login:login' => 'Login',
+		 'list:mails' => 'Mails List',
+		 'user:settings' => 'Account Settings',
+		 'mail:id' => 'Mail id',
+		 'api:api' => 'Api',
+		 'message:message' => 'Message',
+		 'date:date' => 'Date',
+		 'message:to' => 'Message to',
+		 'api:id' => 'Api id',
+		 'api:secret' => 'Secret',
+		 'api:email' => 'Email',
+		 'api:owner' => 'Owner',
+		 'page:previous' => 'Previous',
+		 'page:first' => 'First',
+		 'page:next' => 'Next',
+		 'page:last' => 'Last',
+		 'del:selc' => 'Delete selected',
+		 'apistat:message' => 'This will be part of Buddyexpress Managment V1.1 ( next release ) ',
+		 'login:username' => 'Username',
+		 'login:password' => 'Password',
+		 'login:valid:username' => 'Please enter a valid username',
+		 'login:valid:password' => 'Please enter a password',
+		 'loign:notvalid:user' => 'Cannot login to server',
+		 'nav:home' => 'Home',
+		 'nav:mails' => 'Mails',
+		 'nav:api:add' => 'Add Api',
+		 'nav:apis' => 'Api(s)',	 	 
+		 'nav:settings' => 'Setting(s)',
+		 'logout' => 'Logout',
+		 'hello' => 'Hello ',
+		 'date' => 'Date: ',
+		 'ip' => 'IP: ',
+		 'logout' => 'Logout',
+
+);
+
+bframework_register_languages($englsih);
+?>
