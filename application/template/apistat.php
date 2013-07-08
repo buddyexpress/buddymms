797c4a48c26b3deec931ffdfbc8be7615470156c
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:07:15 -0700

    api stats

---
+++ application/template/apistat.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+ 
+echo '<P>'.bframework_print("apistat:message").'<p>';
