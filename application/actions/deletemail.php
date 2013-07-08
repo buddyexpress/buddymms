afd3b7c1f7a41b7d2b82af93a605536f15b59adf
Arsalan <arsalan@buddyexpress.net>
2013-07-08 09:59:45 -0700

    action for delete mail

---
+++ application/actions/deletemail.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+$delete = new BMMS_APISTATS;
+$delete->delete_mail($_POST['bmms_mails']);
+redirect('page/mails');
