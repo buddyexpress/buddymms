3cdb3c26f58f3d379857cd5638d549b117a1d4a3
Arsalan <arsalan@buddyexpress.net>
2013-07-08 09:59:55 -0700

    action for login

---
+++ application/actions/login.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+user_login(array(
+'username' => $_POST['username'],
+'password' => $_POST['password']
+));
+
