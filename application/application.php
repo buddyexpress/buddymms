f884119482e241f5d4b4efb15312988f5fd26234
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:00:37 -0700

    Application info

---
+++ application/application.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+ 
+/*
+* The application version
+*/ 
+$BframeworkVersion = '1.0-dev';
+/*
+* The application release
+*/ 
+$BframeworkRelease = 'July 8 2013';
+/*
+* The Application Name
+*/ 
+$BframeworkAppName = 'BMMS';
+/*
+* The Application admin url
+* example  admin@mysite.com
+*/ 
+$BframeworkadminEmail = '';
