c6d0f51c8f9ade55952792ff6bf906abc1899313
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:08:56 -0700

    form api add

---
+++ application/views/forms/apiadd.php
@@ -0,0 +1,10 @@
+<div class="bmms-forms">
+<P>
+<input type="text" name="email" placeholder=" Email" />
+</p>
+<p>
+<input type="text" name="owner" placeholder=" Owner Name" />
+</p>
+<input type="submit" value="Add"  class="submit"/>
+</div>
+
