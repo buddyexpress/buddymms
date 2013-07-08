855518b02adb6ff67a26d99ce8367561c128dccf
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:09:36 -0700

    head

---
+++ application/views/head.php
@@ -0,0 +1,4 @@
+
+<script type="text/javascript" src="<?php echo bframework_get_url();?>bframework/js.js?bframework_corejs=jquery/jquery-1.6.4.min"> </script>
+<link rel="stylesheet" href="<?php echo bframework_get_appurl();?>vendors/fancybox/jquery.fancybox-1.3.4.css" type="text/css"/>
+<script type="text/javascript" src="<?php echo bframework_get_appurl();?>vendors/fancybox/jquery.fancybox-1.3.4.js"> </script>
