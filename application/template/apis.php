fd785fe27d8c3cdeb34348063f543898fb71133f
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:07:10 -0700

    apis view

---
+++ application/template/apis.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+ 
+$params = array(
+'attributes' => array(
+                       'action' => bframework_get_url().'action/apidelete',
+                        'method' => 'POST',
+                         ),
+'body' => bframework_view_form('apis'));
+						 
+echo bframework_form($params);
