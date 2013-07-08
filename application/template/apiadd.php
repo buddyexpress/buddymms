d699dc384e429d7a6ec49a8fe924c85065088a47
Arsalan <arsalan@buddyexpress.net>
2013-07-08 10:06:55 -0700

    apis form

---
+++ application/template/apiadd.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+$params = array(
+'attributes' => array(
+                       'action' => bframework_get_url().'action/apiadd',
+                        'method' => 'POST',
+                         ),
+'body' => bframework_view_form('apidadd'));
+						 
+echo bframework_form($params);
