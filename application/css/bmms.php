4ed58e0b6a32683fadb273e291d0353662b8c92d
Leonard Walter <arsalan@buddyexpress.net>
2013-08-16 21:09:45 +0500

    Update bmms.php

--- application/css/bmms.php
+++ application/css/bmms.php
@@ -1,3 +1,13 @@
+/**
+ * Buddyexpress Mail Managment System
+ *
+ * @package   Bframework
+ * @subpackage BMMS
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2013 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
 body {
 padding-bottom: 10px;
 position: relative;
